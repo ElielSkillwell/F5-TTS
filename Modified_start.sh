7a3423b9d47f4b40e940c2814201937e63c52b61
Eliel Taskinen <132335968+ElielSkillwell@users.noreply.github.com>
2024-10-15 10:35:40 +0300

    Corrected the python command

--- Modified_start.sh
+++ Modified_start.sh
@@ -16,8 +16,11 @@ pip install -r requirements.txt
 
 # Example cmd:
  
- python inference-cli.py 
+python inference-cli.py \
     --model "F5-TTS" \
     --ref_audio "reference_data/wheatley.mp3" \
     --ref_text "And 40 potato batteries. Embarrassing. I mean, I realize they're children, but still, you know, low hanging fruit and all that. Barely science really is it?" \
     --gen_text "Generative AI is one of the most groundbreaking innovations of recent years. By leveraging machine learning models, it enables the creation of new data, such as text, images, music, and much more."
+
+
+deactivate
